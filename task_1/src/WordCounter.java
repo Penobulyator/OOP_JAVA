2115f86ed17894d20329f9484427e9fb9b723a53
penobulyator <dima.onishuk70@gmail.com>
2020-02-20 15:25:15 +0700

    Fixed WordCounter

--- task_1/src/WordCounter.java
+++ task_1/src/WordCounter.java
@@ -1,80 +1,104 @@
 import com.sun.istack.internal.NotNull;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
-public class WordCounter
-{
-    private String file_in;
-    private String file_out;
-    private Map<String, Integer> words;
-    private int word_count = 0;
-    private Writer writer;
-    private Reader reader;
+import java.util.stream.Collectors;
 
-    public WordCounter(String file_in, String file_out)
-    {
-        try
-        {
-            this.reader = new InputStreamReader(new FileInputStream(file_in));
-        }
-        catch (IOException ex)
-        {
+public class WordCounter {
+    private Map<String, Integer> _words = new TreeMap<>();
+    private int _wordCount = 0;
+    private Writer _writer;
+    private Reader _reader;
+
+    public WordCounter(String fileIn, String fileOut) {
+        try {
+            this._reader = new FileReader(fileIn);
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-        try
-        {
-            this.writer = new OutputStreamWriter(new FileOutputStream(file_out));
-        }
-        catch (IOException ex)
-        {
+        try {
+            this._writer = new FileWriter(fileOut);
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
-    public WordCounter()
-    {
-        this.reader = new InputStreamReader(System.in);
-        this.writer = new OutputStreamWriter(System.out);
+
+    public WordCounter() {
+        this._reader = new InputStreamReader(System.in);
+        this._writer = new OutputStreamWriter(System.out);
     }
-    private void ProcessLine(String line)
-    {
-        int last_processed_index = 0;
-        for (int i=0;i<line.length();i++)
-        {
-            if (!Character.isLetterOrDigit(line.codePointAt(i)))
-            {
-                char[] cur_word = null;
-                line.getChars(last_processed_index, i, cur_word, 0);
-                Integer cur_count = words.get(cur_word.toString());
-                words.put(cur_word.toString(), cur_count + 1);
-                last_processed_index = i + 1;
-                word_count++;
+
+    private void processLine(String line) {
+        StringBuilder cur_word = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+            if (!Character.isLetterOrDigit(line.codePointAt(i))) {
+                if (cur_word.length() != 0) {
+                    _words.putIfAbsent(cur_word.toString(), 0);
+                    _words.replace(cur_word.toString(), _words.get(cur_word.toString()) + 1);
+                    cur_word = new StringBuilder();
+                    _wordCount++;
+                }
+            } else {
+                cur_word.append(line.charAt(i));
             }
         }
     }
-    public void count()
-    {
-        while(true)
-        {
-            String s = reader.toString();
-            if (s == null)
-                break;
-            else
-            {
-                ProcessLine(s);
+
+    public void count() {
+        BufferedReader br = new BufferedReader(_reader);
+        String s = "";
+        while (true) {
+            try {
+                s = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (s == null) {
+                try {
+                    _reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return;
+            } else {
+                processLine(s);
             }
         }
     }
-    public void print()
-    {
-        //надо дописать
+
+    public void print() {
+        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(_words.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Collections.reverse(list);
+        BufferedWriter bw = new BufferedWriter(_writer);
+        for (Map.Entry<String, Integer> a : list) {
+            try
+            {
+                bw.write(a.getKey()
+                        + ", " + a.getValue()
+                        + ", " + String.format("%.2f", (a.getValue().doubleValue() / _wordCount)*100).replace(',','.') + "%"
+                        + "\r\n");
+                bw.flush();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            _writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public static void main(String[] args)
-    {
-        WordCounter wc = null;
-        if (args.length > 1)
-            wc = new WordCounter(args[1], args[2]);
+
+    public static void main(String[] args) {
+        WordCounter wc;
+        if (args.length == 2)
+            wc = new WordCounter(args[0], args[1]);
         else
             wc = new WordCounter();
         wc.count();
+        wc.print();
     }
 }
